642f5fefbbce6a3832f3ff1c1edc6c5943778042
Kishor Prins <me@68x.ca>
2021-01-27 09:08:39 -0500

    Changed start up script to also load an optional OpenRGB profile at launch

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-exec /usr/src/openrgb/OpenRGB
+default=$(cat /config/default.txt)
+
+if [ -z "$default" ]
+then
+	exec /usr/src/openrgb/OpenRGB
+else
+	exec /usr/src/openrgb/OpenRGB --server --gui --profile $default
+fi
