38d2f03cda4c420a9cbc6c208fce0baab3d44e52
Kishor Prins <kprinssu@users.noreply.github.com>
2021-02-07 08:38:30 -0500

    Use an environment variable called DEFAULT_PROFILE

--- startapp.sh
+++ startapp.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-default=$(cat /config/default.txt)
+#!/bin/bash
 
-if [ -z "$default" ]
+if [ -z "${DEFAULT_PROFILE}" ]
 then
 	exec /usr/src/openrgb/OpenRGB
 else
-	exec /usr/src/openrgb/OpenRGB --server --gui --profile $default
+	exec /usr/src/openrgb/OpenRGB --server --gui --profile "${DEFAULT_PROFILE}"
 fi
