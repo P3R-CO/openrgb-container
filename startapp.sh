6cdfa981affecda162457ea8cfbef84b6efdd7b5
Velekh <85423985+Velekh@users.noreply.github.com>
2022-01-05 09:59:46 +0100

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/src/openrgb/OpenRGB --gui --server --server-port "${SVR_PORT}" --profile "${DEFAULT_PROFILE}"
+exec /usr/src/openrgb/OpenRGB --gui --server --server-port 6742 --profile "${DEFAULT_PROFILE}"
